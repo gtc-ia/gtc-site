626e1fa81ccf9a7f8da8a168f834fa8e7aa6f486
Konstantin Filipenko |  GTC IT <31473848+kfilipenko@users.noreply.github.com>
2025-09-19 22:15:53 +0300

    Update Hero3DCanvas.tsx

--- src/components/Hero3DCanvas.tsx
+++ src/components/Hero3DCanvas.tsx
@@ -2,53 +2,53 @@ import * as React from 'react'
 import * as THREE from 'three'
 import { Canvas, useFrame } from '@react-three/fiber'
 
-function Rotator({ speed = 0.2, children }: React.PropsWithChildren<{ speed?: number }>) {
-  const ref = React.useRef<THREE.Group>(null!)
-  useFrame((_, dt) => {
-    if (!ref.current) return
-    ref.current.rotation.y += dt * speed
-  })
-  return <group ref={ref}>{children}</group>
-}
-
 function Scene() {
+  const sphereRef = React.useRef<THREE.Mesh>(null!)
+  const torus1Ref = React.useRef<THREE.Mesh>(null!)
+  const torus2Ref = React.useRef<THREE.Mesh>(null!)
+  const torus3Ref = React.useRef<THREE.Mesh>(null!)
+
+  useFrame((_, dt) => {
+    if (sphereRef.current) sphereRef.current.rotation.y += dt * 0.25
+    if (torus1Ref.current) torus1Ref.current.rotation.y += dt * 0.10
+    if (torus2Ref.current) torus2Ref.current.rotation.x += dt * 0.10
+    if (torus3Ref.current) torus3Ref.current.rotation.z += dt * 0.10
+  })
+
   return (
     <>
-      {/* Фон и свет */}
       <color attach="background" args={['#0b1220']} />
       <ambientLight intensity={0.6} />
       <pointLight position={[6, 6, 6]} intensity={50} />
       <pointLight position={[-6, -6, -6]} intensity={20} />
 
       {/* Центральная сфера */}
-      <Rotator speed={0.25}>
-        <mesh>
-          <sphereGeometry args={[1.6, 48, 48]} />
-          <meshStandardMaterial
-            color="#67e8f9"
-            emissive="#22d3ee"
-            emissiveIntensity={0.2}
-            metalness={0.1}
-            roughness={0.35}
-          />
-        </mesh>
-      </Rotator>
+      <mesh ref={sphereRef}>
+        <sphereGeometry args={[1.6, 48, 48]} />
+        <meshStandardMaterial
+          color="#67e8f9"
+          emissive="#22d3ee"
+          emissiveIntensity={0.2}
+          metalness={0.1}
+          roughness={0.35}
+        />
+      </mesh>
 
       {/* Три орбиты */}
-      <Rotator speed={0.1}>
-        <mesh rotation={[Math.PI / 2, 0, 0]}>
-          <torusGeometry args={[2.6, 0.02, 8, 128]} />
-          <meshStandardMaterial color="#93c5fd" />
-        </mesh>
-        <mesh rotation={[0, Math.PI / 2, 0]}>
-          <torusGeometry args={[2.0, 0.02, 8, 128]} />
-          <meshStandardMaterial color="#a7f3d0" />
-        </mesh>
-        <mesh rotation={[0, 0, Math.PI / 2]}>
-          <torusGeometry args={[3.2, 0.02, 8, 128]} />
-          <meshStandardMaterial color="#fbcfe8" />
-        </mesh>
-      </Rotator>
+      <mesh ref={torus1Ref} rotation={[Math.PI / 2, 0, 0]}>
+        <torusGeometry args={[2.6, 0.02, 8, 128]} />
+        <meshStandardMaterial color="#93c5fd" />
+      </mesh>
+
+      <mesh ref={torus2Ref} rotation={[0, Math.PI / 2, 0]}>
+        <torusGeometry args={[2.0, 0.02, 8, 128]} />
+        <meshStandardMaterial color="#a7f3d0" />
+      </mesh>
+
+      <mesh ref={torus3Ref} rotation={[0, 0, Math.PI / 2]}>
+        <torusGeometry args={[3.2, 0.02, 8, 128]} />
+        <meshStandardMaterial color="#fbcfe8" />
+      </mesh>
     </>
   )
 }
