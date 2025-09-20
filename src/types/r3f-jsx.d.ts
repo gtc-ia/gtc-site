e892eaeed2fa787d8c2389a92d539e4d7a13a0f6
Konstantin Filipenko |  GTC IT <31473848+kfilipenko@users.noreply.github.com>
2025-09-20 15:27:13 +0300

    Remove ts-nocheck and align R3F JSX typings

--- src/types/r3f-jsx.d.ts
+++ src/types/r3f-jsx.d.ts
@@ -1,11 +1,19 @@
-import '@react-three/fiber';
+import '@react-three/fiber'
+import type { ThreeElements } from '@react-three/fiber'
 
 // Локальная декларация на случай, если окружение не подхватило типы R3F
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      group: any;
+      group: ThreeElements['group']
+    }
+  }
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        group: ThreeElements['group']
+      }
     }
   }
 }
-export {};
+export {}
